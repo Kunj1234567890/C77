261e52eb4ab9c4c45eb0442e9a1f2379f48b488f
Kunj1234567890 <97776880+Kunj1234567890@users.noreply.github.com>
2022-04-08 16:25:52 +0800

    Add files via upload

---
+++ C77/main.js
@@ -0,0 +1,5 @@
+console.log("My first console project");
+           var first_name = "Kunj";
+           var last_name = "Naik";
+           var full_name = first_name.concat(last_name);
+           console.log(full_name);
